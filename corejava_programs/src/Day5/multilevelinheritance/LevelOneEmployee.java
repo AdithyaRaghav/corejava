44a8f90aee23b70f58f2f37a20fac3c97bc02f17
lenovo <lenovo@AdhiRaghav>
2025-10-02 12:56:51 +0530

    commit

---
+++ corejava_programs/src/Day5/multilevelinheritance/LevelOneEmployee.java
@@ -0,0 +1,40 @@
+package Day5.multilevelinheritance;
+
+public class LevelOneEmployee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class LevelOneEmployee extends Employee {
+		    private int noOfShares;
+		    private String authority;
+		    
+		    public LevelOneEmployee() {}
+		    
+		    public LevelOneEmployee(String name, long contactNo, Date dateOfBirth,String deptName, double baseSalary, int noOfShares, String authority) {
+		    	super(name,contactNo,dateOfBirth,deptName, baseSalary);
+		        this.noOfShares = noOfShares;
+		        this.authority=authority;
+		    }
+
+			public int getNoOfShares() {
+				return noOfShares;
+			}
+
+			public void setNoOfShares(int noOfShares) {
+				this.noOfShares = noOfShares;
+			}
+
+			public String getAuthority() {
+				return authority;
+			}
+
+			public void setAuthority(String authority) {
+				this.authority = authority;
+			}
+
+			@Override
+			public String toString() {
+				return "LevelOneEmployee ["+super.toString()+"noOfShares=" + noOfShares + ", authority=" + authority + "]";
+	}
+
+}
