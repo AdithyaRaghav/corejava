e41acc1bae1288c21bcedb5c0eefb80bd47a13bb
lenovo <lenovo@AdhiRaghav>
2025-10-02 12:45:45 +0530

    commit

---
+++ corejava_programs/src/Day5/association/isa/Employee.java
@@ -0,0 +1,24 @@
+package Day5.association.isa;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		private String name;
+		private int employeeId;
+		private String department;
+
+		// constructors
+		public Employee() {
+			super();
+		}
+
+		public Employee(String name, int employeeId, String department) {
+			super();
+			this.name = name;
+			this.employeeId = employeeId;
+			this.department = department;
+
+	}
+
+}
