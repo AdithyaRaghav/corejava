44fd3220bd532c98dc8b5bedf6214515f4b22289
lenovo <lenovo@AdhiRaghav>
2025-10-02 13:15:01 +0530

    commit

---
+++ corejava_programs/src/Day6/usingfinal/FinalMethodClass.java
@@ -0,0 +1,18 @@
+package Day6.usingfinal;
+
+public class FinalMethodClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FinalMethodClass() {
+			System.out.println("This is a default constructor");
+		}
+
+		final int a = 50;
+
+		// Final method
+		final void show() {
+			System.out.println("Value of a: " + a);
+	}
+
+}
