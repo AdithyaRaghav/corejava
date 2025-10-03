045ba3b948b8a2ac6bafdea5d1a30b2f94665234
lenovo <lenovo@AdhiRaghav>
2025-10-03 17:11:02 +0530

    commit

---
+++ corejava_programs/src/Day7/overriding/RBI.java
@@ -0,0 +1,11 @@
+package Day7.overriding;
+
+public class RBI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public float getRateOfInterest() {
+			return 6.7f;
+	}
+
+}
