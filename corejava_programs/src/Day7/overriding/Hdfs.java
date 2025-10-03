ad9ac436a5b5ded07d0a373b8b8b5637041e9b9e
lenovo <lenovo@AdhiRaghav>
2025-10-03 17:07:14 +0530

    commit

---
+++ corejava_programs/src/Day7/overriding/Hdfs.java
@@ -0,0 +1,13 @@
+package Day7.overriding;
+
+public class Hdfs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class HDFC extends RBI {
+			@Override
+			public float getRateOfInterest() {
+				return 6.8f;
+	}
+
+}
