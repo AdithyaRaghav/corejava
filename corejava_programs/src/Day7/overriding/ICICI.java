0f88fabf8ebe31b2460d2a7504026f3ad06c8957
lenovo <lenovo@AdhiRaghav>
2025-10-03 17:08:19 +0530

    commit

---
+++ corejava_programs/src/Day7/overriding/ICICI.java
@@ -0,0 +1,13 @@
+package Day7.overriding;
+
+public class ICICI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class ICICI extends RBI {
+			@Override
+			public float getRateOfInterest() {
+				return 6.9f;
+	}
+
+}
