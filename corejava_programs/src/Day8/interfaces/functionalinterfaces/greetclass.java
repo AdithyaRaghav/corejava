61b8ca628dd183e9a8cc2727a75a288a154b35fa
lenovo <lenovo@AdhiRaghav>
2025-10-04 11:15:40 +0530

    commit

---
+++ corejava_programs/src/Day8/interfaces/functionalinterfaces/greetclass.java
@@ -0,0 +1,17 @@
+package Day8.interfaces.functionalinterfaces;
+
+public class greetclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class GreetClass implements GreetInterface {
+
+			@Override
+			public String greet() {		
+				return "Welcome to the world of Java";
+			}
+
+		}
+	}
+
+}
