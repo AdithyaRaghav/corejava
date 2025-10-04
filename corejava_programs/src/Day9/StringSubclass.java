0968f346476614ff64c0a3c400696e8bf0e6a7cb
lenovo <lenovo@AdhiRaghav>
2025-10-04 12:00:35 +0530

    commit

---
+++ corejava_programs/src/Day9/StringSubclass.java
@@ -0,0 +1,16 @@
+package Day9;
+
+public class StringSubclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class StringSubClass extends String {
+
+			public static void main(String[] args) {
+				
+
+			}
+		}
+	}
+
+}
