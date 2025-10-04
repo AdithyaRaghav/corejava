39a57d5d41186227bd6fcc9a98767906cbc31e7c
lenovo <lenovo@AdhiRaghav>
2025-10-04 11:44:19 +0530

    commit

---
+++ corejava_programs/src/Day8/interfaces/MyInterface.java
@@ -0,0 +1,16 @@
+package Day8.interfaces;
+
+public class MyInterface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public interface MyInterface {
+			 void calculateArea();
+			    interface MyInnerInterface {
+			       int  id = 20;
+			       void print();     
+			    }
+		}
+	}
+
+}
