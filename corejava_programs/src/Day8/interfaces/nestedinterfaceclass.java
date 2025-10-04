cf4bbcc10582bf100368d586b5367961355c051a
lenovo <lenovo@AdhiRaghav>
2025-10-04 11:45:49 +0530

    commit

---
+++ corejava_programs/src/Day8/interfaces/nestedinterfaceclass.java
@@ -0,0 +1,20 @@
+package Day8.interfaces;
+
+public class nestedinterfaceclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class NestedInterfaceClass implements MyInterface.MyInnerInterface{
+
+			@Override
+			public void print() {
+				// TODO Auto-generated method stub
+				 System.out.println("Print method of nested interface");
+			}
+
+			
+
+		}
+	}
+
+}
