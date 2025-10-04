3866e3f121abd408a99b0feb05f8cb122baef7e5
lenovo <lenovo@AdhiRaghav>
2025-10-04 11:12:16 +0530

    commit

---
+++ corejava_programs/src/Day8/interfaces/extendinginterfaces/interfaceone.java
@@ -0,0 +1,12 @@
+package Day8.interfaces.extendinginterfaces;
+
+public class interfaceone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		interface InterfaceOne{  
+			  void print();  
+			}  
+	}
+
+}
