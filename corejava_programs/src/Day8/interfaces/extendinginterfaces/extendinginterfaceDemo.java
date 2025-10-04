fd5e8000adbec68acfdf8b212dbf0fc524a3ac93
lenovo <lenovo@AdhiRaghav>
2025-10-04 11:10:51 +0530

    commit

---
+++ corejava_programs/src/Day8/interfaces/extendinginterfaces/extendinginterfaceDemo.java
@@ -0,0 +1,19 @@
+package Day8.interfaces.extendinginterfaces;
+
+public class extendinginterfaceDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class ExtendingInterfaceDemo implements ChildInterface {
+			// Override ChildInterface method
+			public void print() { 
+				System.out.println("print method");
+			}
+
+			// Override InterfaceOne method
+			public void show() {
+				System.out.println("show Method");
+			}
+	}
+
+}
