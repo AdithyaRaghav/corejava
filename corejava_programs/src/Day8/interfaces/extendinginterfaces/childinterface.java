d916d62dffcf99ab0aa6c504886643a1b3f51611
lenovo <lenovo@AdhiRaghav>
2025-10-04 11:09:39 +0530

    commit

---
+++ corejava_programs/src/Day8/interfaces/extendinginterfaces/childinterface.java
@@ -0,0 +1,12 @@
+package Day8.interfaces.extendinginterfaces;
+
+public class childinterface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public interface ChildInterface extends InterfaceOne {
+			void show();
+		}
+	}
+
+}
