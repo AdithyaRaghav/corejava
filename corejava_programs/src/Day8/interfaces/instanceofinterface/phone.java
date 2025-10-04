f1ad710a7bf5f16ce5f87f461a7230feb6867785
lenovo <lenovo@AdhiRaghav>
2025-10-04 11:31:29 +0530

    commit

---
+++ corejava_programs/src/Day8/interfaces/instanceofinterface/phone.java
@@ -0,0 +1,14 @@
+package Day8.interfaces.instanceofinterface;
+
+public class phone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public interface Phone {
+			void call();
+
+			void sms();
+		}
+	}
+
+}
