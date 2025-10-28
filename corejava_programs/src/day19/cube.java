7b95b69ba82453d390ed5675c948882ea5f2e4fa
lenovo <lenovo@AdhiRaghav>
2025-10-28 11:53:02 +0530

    commit

---
+++ corejava_programs/src/day19/cube.java
@@ -0,0 +1,13 @@
+package day19;
+
+public class cube {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		interface Cube {
+			int calculate(int a); // only one abstract method
+			//int sqr(int n);
+		}
+	}
+
+}
