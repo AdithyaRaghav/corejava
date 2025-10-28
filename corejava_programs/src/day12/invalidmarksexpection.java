b3eaa040e7385afe9901d4f03ab8d6663a49e959
lenovo <lenovo@AdhiRaghav>
2025-10-28 10:53:46 +0530

    commit

---
+++ corejava_programs/src/day12/invalidmarksexpection.java
@@ -0,0 +1,21 @@
+package day12;
+
+public class invalidmarksexpection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class InvalidMarksException extends Exception{
+
+			public InvalidMarksException() {
+				super();
+			}
+
+			public InvalidMarksException(String message) {
+				super(message);
+			}
+
+			
+		}
+	}
+
+}
