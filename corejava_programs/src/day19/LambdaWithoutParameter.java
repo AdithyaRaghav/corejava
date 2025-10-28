9fe88db2be3a8aee9ce7ab3ecb45f867f893b910
lenovo <lenovo@AdhiRaghav>
2025-10-28 12:01:06 +0530

    commit

---
+++ corejava_programs/src/day19/LambdaWithoutParameter.java
@@ -0,0 +1,20 @@
+package day19;
+
+public class LambdaWithoutParameter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class LambdaWithoutParameter {
+
+			public static void main(String[] args) {
+				
+				IStatement s = () -> {return "Hello World!!";};
+				System.out.println(s.show());
+
+
+			}
+
+		}
+	}
+
+}
