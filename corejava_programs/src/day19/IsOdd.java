0ac3bbcaf646b8f343d2e97623373d9870fb8443
lenovo <lenovo@AdhiRaghav>
2025-10-28 11:55:40 +0530

    commit

---
+++ corejava_programs/src/day19/IsOdd.java
@@ -0,0 +1,12 @@
+package day19;
+
+public class IsOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		interface IsOdd{
+			public boolean CheckOdd(int a);
+		}
+	}
+
+}
