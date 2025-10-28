51601355851debb08473da0a99e6cfaadb582b55
lenovo <lenovo@AdhiRaghav>
2025-10-28 12:02:26 +0530

    commit

---
+++ corejava_programs/src/day19/Message.java
@@ -0,0 +1,12 @@
+package day19;
+
+public class Message {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public interface Message{  
+		    public void greet(String name);    
+		 } 
+	}
+
+}
