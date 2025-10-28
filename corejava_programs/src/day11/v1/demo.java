4a0ea238eb95442f8d4632a1c041ffe2e4b71d84
lenovo <lenovo@AdhiRaghav>
2025-10-28 10:37:08 +0530

    commit

---
+++ corejava_programs/src/day11/v1/demo.java
@@ -0,0 +1,17 @@
+package day11.v1;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class Demo {
+
+			public static void main(String[] args) {
+				System.out.println("the program continues....");
+				int data=100/0;
+			}
+			
+		}
+	}
+
+}
