9a32e533795aaf691d3ca8f940998bc491deab08
lenovo <lenovo@AdhiRaghav>
2025-10-28 11:47:06 +0530

    commit

---
+++ corejava_programs/src/InsufficientBalanceException/InsufficientBalanceException.java
@@ -0,0 +1,19 @@
+package InsufficientBalanceException;
+
+public class InsufficientBalanceException {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class InsufficientBalanceException extends Exception {
+
+			public InsufficientBalanceException() {
+				super("Insufficient balance in your account");
+			}
+
+			public InsufficientBalanceException(String message) {
+				super(message);
+			}
+		}
+	}
+
+}
