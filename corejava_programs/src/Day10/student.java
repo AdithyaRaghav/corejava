38e3a517164a5b33b2ab785fc558b9f547e7bb93
lenovo <lenovo@AdhiRaghav>
2025-10-28 10:22:29 +0530

    commit

---
+++ corejava_programs/src/Day10/student.java
@@ -0,0 +1,34 @@
+package Day10;
+
+public class student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class Student {
+			private int rollNo;
+			private String name;
+
+			Student(int rollNo, String name) {
+				this.rollNo = rollNo;
+				this.name = name;
+			}
+
+			public int getRollNo() {
+				return rollNo;
+			}
+
+			public void setRollNo(int rollNo) {
+				this.rollNo = rollNo;
+			}
+
+			public String getName() {
+				return name;
+			}
+
+			public void setName(String name) {
+				this.name = name;
+			}
+		}
+	}
+
+}
