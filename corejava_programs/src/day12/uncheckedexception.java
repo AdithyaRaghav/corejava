7895a2e5c0a63a5e2ded4311fe9fc53bd71d622c
lenovo <lenovo@AdhiRaghav>
2025-10-28 10:57:43 +0530

    commit

---
+++ corejava_programs/src/day12/uncheckedexception.java
@@ -0,0 +1,20 @@
+package day12;
+
+public class uncheckedexception {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class UncheckedException {
+			public static void main(String[] args) {
+				int intArray[];
+				//try {
+					intArray = new int[] { 1, 2, 3, 4 };
+					System.out.println(intArray[6]);
+				//} catch (ArrayIndexOutOfBoundsException e) {
+					//System.err.println("Specified index does not exist. "+e.getMessage());
+				//}
+			}
+		}
+	}
+
+}
