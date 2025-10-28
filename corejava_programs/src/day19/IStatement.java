bd081dd055976111e619eef6564ec737af2b1028
lenovo <lenovo@AdhiRaghav>
2025-10-28 11:54:07 +0530

    commit

---
+++ corejava_programs/src/day19/IStatement.java
@@ -0,0 +1,13 @@
+package day19;
+
+public class IStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		interface IStatement {
+			public String show();// abstract Method
+			
+		}
+	}
+
+}
