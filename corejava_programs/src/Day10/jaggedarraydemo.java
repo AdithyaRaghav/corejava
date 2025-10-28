ce48bb531fe1fed7bc81f6eb46d3699245db1513
lenovo <lenovo@AdhiRaghav>
2025-10-28 10:16:59 +0530

    commit

---
+++ corejava_programs/src/Day10/jaggedarraydemo.java
@@ -0,0 +1,21 @@
+package Day10;
+
+public class jaggedarraydemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class JaggedArrayDemo {
+
+			public static void main(String[] args) {
+
+				int d[][] = { { 10, 20, 30 }, { 40 }, { 12, 21 }, { 11 } };
+
+				System.out.println("Total Rows in Array d : " + d.length);
+
+				MLArray.printArray(d);
+			}
+
+		}
+	}
+
+}
